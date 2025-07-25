bd5f20554f14521bcb60b7e90204ae19c6451d35
sreejareddy624 <245122749010@mvsrec.edu.in>
2025-07-25 10:10:38 +0530

    Add files via upload

---
+++ Arith.java
@@ -0,0 +1,16 @@
+public class Arith
+{
+	public static void main(String args[])
+	{
+		int r1=10;
+		int r2=5;
+		int sum=r1+r2;
+		int diff=r1-r2;
+		int mul=r1*r2;
+		int div=r1/r2;
+		System.out.println("sum="+sum);
+		System.out.println("dif="+diff);
+		System.out.println("mul="+mul);
+		System.out.println("div="+div);
+	}
+}
